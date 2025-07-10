b07e491dae0d84b1c9bf5c32aca9214ffb116eff
离披 <lipi@shuise.dev>
2025-07-11 00:34:40 +0800

    添加项目文件。

---
+++ PluginMaster/Program.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+using PluginMaster;
+
+const string PLUGINS_DIRECTORY = @"K:\repos\URA-Plugins\";
+var plugins = new List<PluginInformation>();
+foreach (var path in Directory.EnumerateFiles(PLUGINS_DIRECTORY, "manifest.json", SearchOption.AllDirectories))
+{
+    if (!path.Contains("bin\\Debug\\net8.0")) continue;
+    plugins.Add(JsonConvert.DeserializeObject<PluginInformation>(File.ReadAllText(path))!);
+}
+File.WriteAllText("../../../manifests.json", JsonConvert.SerializeObject(plugins, Formatting.Indented));
